9824f48554e85547e0b7ab5a891df2838396fd37
30Cents98 <65137583+30Cents98@users.noreply.github.com>
2020-05-10 18:55:39 -0500

    Add files via upload

---
+++ KOTOR2Bingo.js
@@ -0,0 +1,184 @@
+var bingoListVersion = "1.0";
+var bingoList = [];
+
+bingoList[1] = [
+  { name: "Get Hanharr", types: ["Character"] },
+  { name: "Get Mira", types: ["Character"] },
+  { name: "Get Disciple/Handmaiden to jedi", types: ["Character," "Influence"] },
+  { name: "Make Bao-Dur a jedi", types: ["Character," "Influence] },
+  { name: "Force Vision", types: ["Force Power," "Influence"] },
+  { name: "Implant Level 3", types: ["Character"] },
+  { name: "100 Alignment", types: ["Alignment"] },
+  { name: "0 Alignment", types: ["Alignment"] },
+];
+bingoList[2] = [
+  { name: "0 influence with 1 character", types: ["Influence"] },
+  { name: "Break Hanharr's Spirit", types: ["Character," "Influence"] },
+  { name: "Make Mira a jedi", types: ["Character," "Influence"] },
+  { name: "Go to the Rebuilt Jedi Enclave with none of the masters there", types: ["Dantooine"] },
+  { name: "Kill Atris", types: ["Telos"] },
+  { name: "All Unique Robes", types: ["Robes"] },
+  { name: "Exactly 0 Credits", types: ["Credits"] },
+  { name: "Get HK-47", types: ["Character"] },
+];
+bingoList[3] = [
+  { name: "Complete the Sith Tomb on Korriban", types: ["Korriban"] },
+  { name: "Learn Moving Meditation", types: ["Force Power, " "Influence"] },
+  { name: "Get main character on title screen", types: ["Alignment"] },
+  { name: "Install HK-47's unique module", types: ["Character"] },
+  { name: "Defeat everyone in the Pazaak Den", types: ["Pazaak"] },
+  { name: "Free Ramana", types: ["Telos"] },
+  { name: "Dance for Vogga the Hutt", types: ["Nar Shaddaa"] },
+  { name: "Complete all prologue quests", types: ["Ebon Hawk"] },
+];
+bingoList[4] = [
+  { name: "Complete all Telos quests", types: ["Telos"] },
+  { name: "Complete all Dantooine quests", types: ["Dantooine"] },
+  { name: "Complete all Nar Shaddaa quests", types: ["Nar Shaddaa"] },
+  { name: "Complete all Dxun quests", types: ["Dxun"] },
+  { name: "Complete all Onderon quests", types: ["Onderon"] },
+  { name: "Learn Force Storm", types: ["Force power"] },  
+  { name: "Kill all HK droids"", types: ["HK"] },
+  { name: "Get any prestige class", types: ["Prestige"] },
+];
+bingoList[5] = [
+  { name: "Get your pet crystal", types: ["Crystal"] },
+  { name: "Play on \"difficult"\ ", types: ["Difficulty"] },  
+  { name: "Opila Crystal", types: ["Crystal"] },
+  { name: "Complete the Mandalorian battle circle without violating the rules", types: ["Dxun"] },
+  { name: "Complete the Handmaiden Echani battle circle", types: ["Telos"] },
+  { name: "Win all swoop races", types: ["Swoop"] },  
+  { name: "Play against every pazaak player at least once", types: ["Pazaak"] },
+  { name: "Side with Chodo Habat", types: ["Telos"] },  
+];
+bingoList[6] = [
+  { name: "Side with Jana Lorso", types: ["Telos"] },
+  { name: "Find out the Disciple's true name", types: ["Disciple"] },
+  { name: "Kill Sion", types: ["Malachor"] },
+  { name: "Obtain Darth Nihlus' Mask", types: ["Ravager"] },
+  { name: "Kill Colonel Tobin", types: ["Ravager"] },
+  { name: "Two Weapon Specializations", types: ["Character"] },
+  { name: "Get the Handmaiden to hate you", types: ["Handmaiden"] },
+  { name: "Complete all fights with the Handmaiden", types: ["Handmaiden"] },
+];
+bingoList[7] = [
+  { name: "Complete all of G0-T0's bounties", types: ["G0-T0"] },
+  { name: "Free Adana", types: ["Nar Shaddaa"] },
+  { name: "Find out G0-T0's true identity", types: ["G0-T0"] },
+  { name: "250 base Force Points", types: ["Crystal"] },
+  { name: "Convince Vogga the Hutt to sell fuel to Telos", types: ["Nar Shaddaa"] },
+  { name: "Silver Crystal", types: ["Crystal"] },
+  { name: "Get a fully upgraded blaster", types: ["Blaster"] },
+  { name: "Clear out all Sith Strongholds", types: ["Clear"] },
+];
+bingoList[8] = [
+  { name: "21 base Charisma", types: ["Attribute"] },
+  { name: "Kill a Zakkeg", types: ["Dxun"] },
+  { name: "Defeat Hanharr twice", types: ["Nar Shaddaa, " "Malachor"] },
+  { name: "Kill the 2 big beasts", types: ["Onderon, " "Malachor"] },
+  { name: "Complete 30 quests", types: ["Quest"] },
+  { name: "Fully Repair HK-47", types: ["Party","HK"] },
+  { name: "Repair the speeder on Nar Shaddaa", types: ["Nar Shaddaa"] },
+  { name: "Spare  all jedi masters", types: ["Quest"] },  
+];
+bingoList[9] = [
+  { name: "Fully upgraded lightsaber", types: ["lightsaber","Upgrade"] },
+  { name: "Complete Freedon Nadd's Tomb without skipping it", types: ["Dxun","Tombs"] },
+  { name: "Kill everyone in the Jekk Jekk Tarr", types: ["Nar Shaddaa"] },
+  { name: "Find ships for all Nar Shaddaa refugees", types: ["Nar Shaddaa"] },
+  { name: "Get to 30 computer use", types: ["Character","Skills"] },
+  { name: "Get to 30 persuade", types: ["Character","Skills"] },
+  { name: "Get to 30 security", types: ["Character","Skills"] },
+  { name: "Get to 30 treat injury", types: ["Character","Skills"] },  
+  { name: "Get to 30 awareness", types: ["Character","Skills"] },  
+];
+bingoList[10] = [
+  { name: "Get to 30 demolitions", types: ["Character","Skills"] },
+  { name: "Get to 30 repair", types: ["Character","Skills"] },  
+  { name: "Get to 30 stealth", types: ["Character","Skills"] },
+  { name: "Rescue the Ithorians from Czerka", types: ["Telos"] },  
+  { name: "Romance with Visas", types: ["Party", "Gender"] },
+  { name: "Side with Queen Talia on Onderon", types: ["Onderon"] },
+  { name: "Side with General Vaklu", types: ["Onderon"] },
+  { name: "Kill all generals on Onderon", types: ["Onderon"] },
+];
+bingoList[11] = [
+  { name: "Qixoni Crystal", types: ["Crystal"] },
+  { name: "Side with the protestors on Onderon", types: ["Onderon"] },
+  { name: "Kill Kumus", types: ["Dxun"] },
+  { name: "Kill all bounty hunters on Onderon", types: ["Onderon"] },
+  { name: "Get the -T card", types: ["Pazaak"] },  
+  { name: "Complete all of Samhan's requests", types: ["Telos"] },
+  { name: "All Master-Level Blaster Feats", types: ["Character"] },
+  { name: "All lightsaber crystal colors", types: ["Lightsaber"] },
+];
+bingoList[12] = [
+  { name: "Download All Area Schematics on Telos Station", types: ["Telos"] },
+  { name: "Have Bao-Dur unlock the sensors on Dantooine", types: ["Dantooine"] },  
+  { name: "Learn the history of all the sith lords on Korriban", types: ["Korriban"] },
+  { name: "Master Caution, Empathy, and Gear Head", types: ["Character"] },
+  { name: "Learn all lightsaber forms", types: ["Forms"] },
+  { name: "Get all party members", types: ["Party"] },
+  { name: "Purchase a Store's Entire Inventory", types: ["Credits"] },
+  { name: "Have 100,000 credits", types: ["Credits"] },  
+];
+bingoList[13] = [
+  { name: "Break into the secret Manadlorian stash", types: ["Dxun"] },
+  { name: "Break into the the locked door in the Sith Academy", types: ["Korriban"] },  
+  { name: "Kill Master Vrook", types: ["Dantooine"] },
+  { name: "Kill Master Zez Kai-El", types: ["Nar Shaddaa"] },
+  { name: "Kill Master Kavar", types: ["Onderon"] },
+  { name: "All 3 Force Immunity Feats", types: ["Character"] },  
+  { name: "Discover Vash's Body on Korriban", types: ["Korriban"] },
+  { name: "Change your mind about siding with the Settlers at Khoonda", types: ["Dantooine"] },
+];
+bingoList[14] = [
+  { name: "Side with the Settlers at Khoonda", types: ["Dantooine"] },
+  { name: "Save Joran", types: ["Dantooine"] },
+  { name: "Kill Joran", types: ["Dantooine"] },
+  { name: "One of All 4 Deadly Mines", types: [] },
+  { name: "Purchase what Joran found in the Enclave", types: ["Dantooine"] },
+  { name: "Force persuade the thugs to jump into the pit", types: ["Nar Shaddaa"] },
+  { name: "Clear out the Kinrath Cave", types: ["Dantooine"] },  
+  { name: "Steal the Kinrath Gland from the mercenary in the cave", types: ["Dantooine"] },
+];
+bingoList[15] = [
+  { name: "Complete All 5 Taris Bounty Quests", types: ["Taris","Bounty"] },
+  { name: "Complete \"Scout Hunt\"", types: ["Dxun"] },
+  { name: "Sabotage Khoonda's defenses", types: ["Dantooine"] },
+  { name: "Complete \"Ghosts to Rest\"", types: ["Dantooine"] },
+  { name: "Turn in Samhan", types: ["Telos"] },
+  { name: "Escort the Twilek to the exit of the Military Base", types: ["Telos"] },  
+  { name: "Clear out G0-T0's Yacht", types: ["Nar Shaddaa"] },
+  { name: "Learn about Atton's past", types: ["Atton"] },
+];
+bingoList[16] = [
+  { name: "Strengthen Khoonda's defnese to its max", types: ["Dantooine"] },
+  { name: "Kill the escaped criminals on Telos Surface", types: ["Telos"] },
+  { name: "Find all the hidden stashes on Telos Surface", types: ["Telos"] },
+  { name: "Complete \"Farm Equipment\"", types: ["Dantooine"] },
+  { name: "Complete \"Chieftain in Need\"", types: ["Party"] },
+  { name: "Clear out the Ravager", types: ["Ravager"] },
+  { name: "250 Maximum Vitality", types: ["Character"] },
+  { name: "Allow Visas into her chamber on the Ravager", types: ["Ravager"] },
+];
+bingoList[17] = [
+  { name: "All Characters Use Blasters Only", types: ["Character","Restriction"] },
+  { name: "Force Meditation", types: ["Power"] },
+  { name: "Force Crush", types: ["Power"] },
+  { name: "Free the beast on Onderon", types: ["Onderon"] },
+  { name: "Give a starport visa to the mom and her children", types: ["Quest"] },
+  { name: "Complete \"Mass Shadow Generator\"", types: ["Malachor] },
+  { name: "250 Maximum Force Points", types: ["Character"] },
+  { name: "Allow all the sith soldiers on board, and then kill them all", types: ["Peragus"] },
+];
+bingoList[18] = [
+  { name: "40 Points in All Skills", types: ["Character"] },
+  { name: "Achieve Level 20", types: ["Character"] },
+  { name: "Learn all Force Powers only obtained from party members", types: ["Force Power"] },
+  { name: "Master Drain Force", types: ["Force Power"] },
+  { name: "Precise Shot V", types: ["Feat"] },
+  { name: "13 Unique Lightsaber Power Crystals", types: ["Crystal", "Collect"] },
+  { name: "Master Duel Strike", types: ["Feat"] },
+  { name: "At Least 25 in All Saves", types: ["Character"] },
+];
